2d4abe8da15a8c34fc8c43d4f788b62292e555a8
Erdi Topuzlu <erditpzl@gmail.com>
2025-01-07 20:53:11 +0300

    Create api operations completed

--- Section 1/accounts/src/main/java/com/eazybytes/accounts/constants/AccountsConstants.java
+++ Section 1/accounts/src/main/java/com/eazybytes/accounts/constants/AccountsConstants.java
@@ -18,4 +18,6 @@ public class AccountsConstants {
     public static final String  STATUS_500 = "500";
     public static final String  MESSAGE_500 = "An error occurred. Please try again or contact Dev team";
 
+    public static final String CUSTOMER_ALREADY_EXISTS = "Customer already registered with this mobile number : ";
+
 }
